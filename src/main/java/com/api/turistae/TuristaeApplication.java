a9bb8a045ffbe30ef75a379acd787dc933ed7715
Ettore Alessandro Rollo <ettorerollo@hotmail.com>
2023-04-29 20:42:42 -0300

    Classe de logs implementada

--- src/main/java/com/api/turistae/TuristaeApplication.java
+++ src/main/java/com/api/turistae/TuristaeApplication.java
@@ -1,13 +1,19 @@
 package com.api.turistae;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TuristaeApplication {
 
+	//  Logger do projeto
+    private static final Logger logger = LoggerFactory.getLogger(TuristaeApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(TuristaeApplication.class, args);
+		logger.info("Projeto iniciado");
 	}
 
 }
