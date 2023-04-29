d1159de4fcba16a337f4c1ff186b0480adad2829
Ettore Alessandro Rollo <ettorerollo@hotmail.com>
2023-04-29 15:36:40 -0300

    commit inicial

---
+++ src/main/java/com/api/turistae/TuristaeApplication.java
@@ -0,0 +1,13 @@
+package com.api.turistae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristaeApplication.class, args);
+	}
+
+}
