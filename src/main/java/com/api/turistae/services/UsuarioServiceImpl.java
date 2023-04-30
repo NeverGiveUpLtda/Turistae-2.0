25cd4ac4212beccf58b401ade08ee97b712780f2
Ettore Alessandro Rollo <ettorerollo@hotmail.com>
2023-04-30 18:43:17 -0300

    Adicionado logger para UsuarioServiceImpl

--- src/main/java/com/api/turistae/services/UsuarioServiceImpl.java
+++ src/main/java/com/api/turistae/services/UsuarioServiceImpl.java
@@ -6,6 +6,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.api.turistae.dtos.DadosUsuarioDTO;
@@ -13,11 +15,7 @@ import com.api.turistae.dtos.UsuarioDTO;
 import com.api.turistae.exceptions.CriptografiaException;
 import com.api.turistae.exceptions.RegraNegocioException;
 import com.api.turistae.models.Usuario;
-import com.api.turistae.repositorys.CurtidaRepository;
-import com.api.turistae.repositorys.ReviewRepository;
-import com.api.turistae.repositorys.TurismoRepository;
 import com.api.turistae.repositorys.UsuarioRepository;
-import com.api.turistae.repositorys.VoucherRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -28,39 +26,39 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     // Atributos
     private final UsuarioRepository usuarioRepository;
-    private final TurismoRepository turismoRepository;
-    private final CurtidaRepository curtidaRepository;
-    private final ReviewRepository reviewRepository;
-    private final VoucherRepository voucherRepository;
+    private final Logger logger = LoggerFactory.getLogger(UsuarioServiceImpl.class);
 
     // Métodos
     @Override
     @Transactional
     public Long post(UsuarioDTO dto) {
 
-        Usuario usu = new Usuario();
-        usu.setNomeUsuario(dto.getNomeUsuario());
+        logger.info("Post usuário: {}", dto.toString());
+
+        Usuario usuario = new Usuario();
+        usuario.setNomeUsuario(dto.getNomeUsuario());
+
         try {
-            usu.setSenha(criptografarSenha(dto.getSenha()));
+            usuario.setSenha(criptografarSenha(dto.getSenha()));
         } catch (CriptografiaException e) {
             throw new RegraNegocioException(e.getMessage());
         }
 
-        usu.setNome(dto.getNome());
-        usu.setEmail(dto.getEmail());
-        usu.setTelefone(dto.getTelefone());
-        usu.setNumeroCasa(dto.getNumeroCasa());
-        usu.setRua(dto.getRua());
-        usu.setBairro(dto.getBairro());
-        usu.setCidade(dto.getCidade());
-        usu.setEstado(dto.getEstado());
-        usu.setDataNascimento(dto.getDataNascimento());
-        usu.setProfissao(dto.getProfissao());
-        usu.setCadastroPessoaFisica(dto.getCadastroPessoaFisica());
-        usu.setRegistroGeral(dto.getRegistroGeral());
-        usu.setDataEdicao(dataAtualMascara("yyyy-MM-dd-HH-mm-ss"));
+        usuario.setNome(dto.getNome());
+        usuario.setEmail(dto.getEmail());
+        usuario.setTelefone(dto.getTelefone());
+        usuario.setNumeroCasa(dto.getNumeroCasa());
+        usuario.setRua(dto.getRua());
+        usuario.setBairro(dto.getBairro());
+        usuario.setCidade(dto.getCidade());
+        usuario.setEstado(dto.getEstado());
+        usuario.setDataNascimento(dto.getDataNascimento());
+        usuario.setProfissao(dto.getProfissao());
+        usuario.setCadastroPessoaFisica(dto.getCadastroPessoaFisica());
+        usuario.setRegistroGeral(dto.getRegistroGeral());
+        usuario.setDataEdicao(getDataAtualComMascara("yyyy-MM-dd-HH-mm-ss"));
 
-        Usuario usuarioGerado = usuarioRepository.save(usu);
+        Usuario usuarioGerado = usuarioRepository.save(usuario);
 
         return usuarioGerado.getId();
 
@@ -120,21 +118,22 @@ public class UsuarioServiceImpl implements UsuarioService {
     }
 
     // Funcção que retorna a data atual com uma máscara
-    private LocalDateTime dataAtualMascara(String mascara) {
+    private LocalDateTime getDataAtualComMascara(String mascara) {
 
         String data = LocalDateTime.now().format(DateTimeFormatter.ofPattern(mascara));
         return LocalDateTime.parse(data, DateTimeFormatter.ofPattern(mascara));
 
     }
 
+    // TODO mudar esse método para o controller
     private String criptografarSenha(String senha) throws CriptografiaException {
         try {
 
             // Crie uma instância da classe MessageDigest com o algoritmo SHA-256
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
 
             // Converta a senha para um array de bytes e realize a criptografia
-            byte[] senhaCriptografada = md.digest(senha.getBytes());
+            byte[] senhaCriptografada = messageDigest.digest(senha.getBytes());
 
             // Converta o array de bytes para uma string hexadecimal
             StringBuilder hexString = new StringBuilder();
@@ -147,6 +146,8 @@ public class UsuarioServiceImpl implements UsuarioService {
 
             return hexString.toString();
         } catch (Exception e) {
+
+            logger.error(e.getMessage(), e);
 
             // Trate o erro caso ocorra
             throw new CriptografiaException("Não foi possível Criptografar a senha");
