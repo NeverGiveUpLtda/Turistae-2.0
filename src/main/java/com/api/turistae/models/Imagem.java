1493e71dc4a82bac2f42b2ef0a607b63cd79af77
Ettore Alessandro Rollo <ettorerollo@hotmail.com>
2023-04-29 23:38:37 -0300

    Implementado Classe imagem

--- src/main/java/com/api/turistae/models/Imagem.java
+++ src/main/java/com/api/turistae/models/Imagem.java
@@ -1,5 +1,51 @@
 package com.api.turistae.models;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Imagem {
+    //  Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(nullable = false)
+    private String url;
+
+    //  Relacionamentos
+    @ManyToOne
+    @JoinColumn(name = "turismo_id", nullable = false)
+    Turismo turismo;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    Usuario usuario;
+
+    //  Timestamps 
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dataCriacao;
+
+    @Column(nullable = false)
+    private LocalDateTime dataEdicao;
     
 }
