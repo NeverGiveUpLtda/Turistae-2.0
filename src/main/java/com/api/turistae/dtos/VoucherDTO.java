16f7810f5004665b7b6fadf2f9722ab017239182
Ettore Alessandro Rollo <ettorerollo@hotmail.com>
2023-04-30 00:15:50 -0300

    Classe VoucherDTO implementada

--- src/main/java/com/api/turistae/dtos/VoucherDTO.java
+++ src/main/java/com/api/turistae/dtos/VoucherDTO.java
@@ -1,5 +1,28 @@
 package com.api.turistae.dtos;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class VoucherDTO {
+
+    //  Atributos
+    private long id;
+    private String codigo;
+
+    //  Relacionamentos
+    private long turismoId;
+    private long usuarioId;
+
+    //  Timestamps
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataEdicao;
     
 }
