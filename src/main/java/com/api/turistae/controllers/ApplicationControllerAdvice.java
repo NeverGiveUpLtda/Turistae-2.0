49661a40a907596d4dd90f0a8d73977a1c6a0ce9
Ettore Alessandro Rollo <ettorerollo@hotmail.com>
2023-05-01 03:39:15 -0300

    Logger para classe de erros

--- src/main/java/com/api/turistae/controllers/ApplicationControllerAdvice.java
+++ src/main/java/com/api/turistae/controllers/ApplicationControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.api.turistae.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,8 +13,15 @@ import com.api.turistae.exceptions.RegraNegocioException;
 @RestControllerAdvice
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ApplicationControllerAdvice {
+
+    // Atributos
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationControllerAdvice.class);
+
+    // Métodos
     @ExceptionHandler(RegraNegocioException.class)
     public ApiError handlerRegraNegocioException(RegraNegocioException ex) {
+        
+        logger.error("Erro de regra de negócio: {}", ex.getMessage());
         return new ApiError(ex.getMessage());
     }
 }
