68cca097013bce3d45dd81f49783bab48639bee0
Ettore Alessandro Rollo <ettorerollo@hotmail.com>
2023-06-16 21:19:35 -0300

    correções

--- src/main/java/com/api/turistae/TuristaeApplication.java
+++ src/main/java/com/api/turistae/TuristaeApplication.java
@@ -4,31 +4,8 @@ import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.api.turistae.exceptions.CriptografiaException;
-import com.api.turistae.models.Categoria;
-import com.api.turistae.models.Curtida;
-import com.api.turistae.models.Imagem;
-import com.api.turistae.models.Review;
-import com.api.turistae.models.Turismo;
-import com.api.turistae.models.Usuario;
-import com.api.turistae.models.Voucher;
-import com.api.turistae.repositorys.CategoriaRepository;
-import com.api.turistae.repositorys.CurtidaRepository;
-import com.api.turistae.repositorys.ImagemRepository;
-import com.api.turistae.repositorys.ReviewRepository;
-import com.api.turistae.repositorys.TurismoRepository;
-import com.api.turistae.repositorys.UsuarioRepository;
-import com.api.turistae.repositorys.VoucherRepository;
-import com.api.turistae.utils.CriptografiaUtils;
-import com.api.turistae.utils.DataUtils;
-import com.api.turistae.utils.VoucherUtils;
-
 @SpringBootApplication
 public class TuristaeApplication {
 
