4e9db691f054e03317b2849638a7bef02ca18bb0
Ettore Alessandro Rollo <ettorerollo@hotmail.com>
2023-06-16 21:20:15 -0300

    correções

--- src/main/java/com/api/turistae/TuristaeApplication.java
+++ src/main/java/com/api/turistae/TuristaeApplication.java
@@ -1,7 +1,5 @@
 package com.api.turistae;
 
-import java.util.Random;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,18 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TuristaeApplication {
 
-    // Atributos
-    private static final String CATEGORIA_SALVA = "Categoria salva: {}";
-    private static final String USUARIO_SALVO = "Usuario salvo: {}";
-    private static final String CURTIDA_SALVA = "Curtida salva: {}";
-    private static final String IMAGEM_SALVA = "Imagem salva: {}";
-    private static final String REVIEW_SALVO = "Review salvo: {}";
-    private static final String TURISMO_SALVO = "Turismo salvo: {}";
-    private static final String VOUCHER_SALVO = "Voucher salvo: {}";
-
     // Logger do projeto
     private static final Logger logger = LoggerFactory.getLogger(TuristaeApplication.class);
-    private static final Random random = new Random();
 
     public static void main(String[] args) {
         SpringApplication.run(TuristaeApplication.class, args);
